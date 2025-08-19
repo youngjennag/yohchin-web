7cff0047b730336c5aa02411d69b4683101d5472
youngjennag <chiny8174@gmail.com>
2025-08-19 17:45:39 +0900

    Add files via upload

---
+++ PHP基礎コース/PHP_5/html/inc_all_input.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>入力した値を表示します。</p>
+  <form action="output.php" method="post">
+    <input type="text" name="word">
+    <input type="submit" value="確定">
+  </form>
+</body>
+</html>
---
+++ PHP基礎コース/PHP_5/output.php
@@ -0,0 +1,7 @@
+<?php
+  require_once("./html/inc_all_input.php");
+  
+  if(isset($_POST["word"])){
+    echo $_POST["word"];
+  }
+?>
